c8f1b4654f90d38bf17ef5a690d0b727749208cf
Hardik Nanda <hnanda21@gmail.com>
2022-04-20 21:28:23 +0530

    Update

---
+++ crud/formProcess.php
@@ -0,0 +1,44 @@
+<?php
+	if (isset($_POST['submit'])) {
+		$user = $_POST['username'];
+		$pass = $_POST['password'];
+
+
+		$connection = mysqli_connect('localhost', 'root', '', 'loginapp'); //making connection to the database
+
+		if ($connection) {
+		} else {
+			die(); //connection failed
+		}
+
+		$query = "INSERT INTO user(username, password) "; 
+		$query .= "VALUES ('$user', '$pass')";
+
+		$result = mysqli_query($connection, $query);
+
+		if (!$result) {
+			die(); 
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	<div class="m-4">
+    <!-- Success Alert -->
+    <div class="alert alert-success alert-dismissible fade show">
+        Congrats! Account created successfuly!
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+	
+</body>
+</html>
